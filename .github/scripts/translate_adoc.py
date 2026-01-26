0be1757003db4becc96a17e512428932ed49561d
Nisamov <160645558+NisamovSandbox@users.noreply.github.com>
2026-01-27 00:26:13 +0100

    Prueba 5

--- .github/scripts/translate_adoc.py
+++ .github/scripts/translate_adoc.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 from pathlib import Path
-from googletrans import Translator
-
+from deep_translator import GoogleTranslator
 import sys
 
-translator = Translator()
-
-SRC_ROOT = Path(sys.argv[1])   # Carpeta de origen (español)
-DST_ROOT = Path(sys.argv[2])   # Carpeta destino (inglés)
+SRC_ROOT = Path(sys.argv[1])
+DST_ROOT = Path(sys.argv[2])
 DST_ROOT.mkdir(exist_ok=True, parents=True)
-
 for src_file in SRC_ROOT.rglob("*.adoc"):
     relative_path = src_file.relative_to(SRC_ROOT)
     dst_file = DST_ROOT / relative_path
     dst_file.parent.mkdir(parents=True, exist_ok=True)
-    
     text = src_file.read_text(encoding="utf-8")
-    translated = translator.translate(text, src='es', dest='en').text
+    translated = GoogleTranslator(source='es', target='en').translate(text)
     dst_file.write_text(translated, encoding="utf-8")
     print(f"Traducido: {src_file} -> {dst_file}")
